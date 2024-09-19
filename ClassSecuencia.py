009d3f58620365db8d7785ec64cd2386c1ee512d
Carlos Barrera <carlosbarrerac121@ciencias.unam.mx>
2024-09-19 12:37:25 -0600

    Método ordenar terminado.
    Método auxiliar terminado, incluye métodos auxiliares para la su definición.

--- ClassSecuencia.py
+++ ClassSecuencia.py
@@ -1,6 +1,6 @@
 # Programa: ClassSecuencia.py
-# Objetivo: Mostrar la implementación de la "interfaz" Conjuntable, para
-#           ilustrar la creación de un TAD de objetos.
+# Objetivo: Mostrar la implementacion de la "interfaz" Conjuntable, para
+#           ilustrar la creacion de un TAD de objetos.
 # Autores: Milena Rivera, Carlos Barrera, Isaac Garrido, Mayela Rosas
 # Version: 18-09-2024
 
@@ -26,7 +26,7 @@ class Secuencia(Ic.Conjuntable):
                 self.__nd = 0
                 print(f"Se creo una Secuencia para {params[0]} elementos!\n")
             except ValueError:
-                print("El tamaño del arreglo debe ser positivo!\n")
+                print("El tamanio del arreglo debe ser positivo!\n")
 
     @property
     def datos(self):
@@ -48,7 +48,8 @@ class Secuencia(Ic.Conjuntable):
 
     def agregar(self, *params):
         """
-        Metodo para agregar un elemento a la Secuencia, siempre que sea posible.
+        Metodo para agregar un elemento a la Secuencia, siempre
+        que sea posible.
         :param params: Si solo recibe un parametro, este debera de ser el elemento a agregar
                        Si recibe dos parametros, estos seran el elemento a agregar y el numero
                        de veces que se desea agregar.
@@ -69,8 +70,8 @@ class Secuencia(Ic.Conjuntable):
 
     def eliminar(self, *params):
         """
-        Metodo que permite eliminar todas las repeticiones del elemento dentro de la Secuencia,
-        siempre que esto sea posible.
+        Metodo que permite eliminar todas las repeticiones del elemento dentro
+        de la Secuencia, siempre que esto sea posible.
         :param params: Si solo recibe un parametro, este debera de ser el elemento a eliminar.
                         (en caso de estar repetido en la secuencia, este metodo eliminara el
                         primero que se encuentre)
@@ -96,8 +97,7 @@ class Secuencia(Ic.Conjuntable):
                                 self.__datos[i] = self.__datos[self.__nd]
                             print(f"El elemento {params[0]} fue eliminado!\n")
                             encontro = True
-                            # elimino el primero que encuentro
-                            break
+                            break # elimino el primero que encuentro
                         i += 1
                 except StopIteration:
                     pass
@@ -107,8 +107,7 @@ class Secuencia(Ic.Conjuntable):
         elif len(params) == 2:
             # Hay que asegurarnos que el arreglo no este vacio y el elemento exista
             if not self.esta_vacia() and self.contiene(params[0]):
-                # me ayudara a contar cuantos llevo eliminados
-                aux = 0
+                aux = 0 # me ayudara a contar cuantos llevo eliminados
                 encontro = False
                 it1 = iter(self)
                 i = 0
@@ -126,8 +125,8 @@ class Secuencia(Ic.Conjuntable):
                             aux += 1
                             if aux == params[1]:
                                 break
-                        # en teoria en la secuencia si hay que seguir buscando
-                        i += 1
+                            else:# en teoria en la secuencia si hay que seguir buscando
+                                i += 1
                 except StopIteration:
                     pass
                 if not encontro:
@@ -136,7 +135,7 @@ class Secuencia(Ic.Conjuntable):
     # carlos
     def contiene(self, elemento: Em.Empleado) -> bool:
         """
-        Metodo que permite saber si un elemento se encuentra contenido
+        Metodo que permite saber si� un elemento se encuentra contenido
         dentro de la Secuencia.
         :param elemento: El elemento a buscar
         :return: True si lo encontro, False en otro caso
@@ -147,11 +146,11 @@ class Secuencia(Ic.Conjuntable):
                 it1 = iter(self)
                 while True:
                     empleado = next(it1)
-                    if empleado == elemento:
+                    if empleado == elemento: #Esta en el arreglo
                         return True
             except StopIteration:
                 pass
-        return False
+        return False        
 
     def repeticiones(self, elemento: Em.Empleado) -> int:
         """
@@ -167,16 +166,16 @@ class Secuencia(Ic.Conjuntable):
             try:
                 while True:
                     empleado = next(it1)
-                    if elemento == empleado:
-                        contador += 1
+                    if elemento == empleado: #Lo encontro
+                        contador +=1
             except StopIteration:
                 pass
         return contador
 
     def esta_vacia(self) -> bool:
         """
-        Metodo que permite saber sí la Secuencia esta vacia.
-        :return: True si esta vacia, False en otro caso.
+        Metodo que permite saber si� la Secuencia esta vaci�a.
+        :return: True si esta vaci�a, False en otro caso.
         :rtype: bool
         """
         return self.__nd == 0
@@ -201,25 +200,29 @@ class Secuencia(Ic.Conjuntable):
         (sin repeticiones).
         :return: La Secuencia sin repetidos
         """
-        copia = Secuencia(len(self.__datos))
+        copia = Secuencia(len[self.__datos])
         it1 = iter(self)
         try:
             while True:
                 elemento = next(it1)
-                if not copia.contiene(elemento):
+                if not copia.contiene(elemento): #Solo lo agrega una vez
                     copia.agregar(elemento)
         except StopIteration:
             return copia
 
-    def ordenar(self):
+    def ordenar(self, inicio, fin, comparador):
         """
         Metodo que permite devuelve la Secuencia de elementos ordenada.
         Utiliza Quick Sort o Merge Sort y habilita la existencia de dos comparadores
         por ejemplo, en el caso de los Empleados, se pueden ordenar por edad, por
         salario, por edad y nombre, salario y nombre, etc.
+        Caso concreto del metodo ordenar_recurivo
         :return: La Secuencia ordenada
         """
-        pass
+        self.ordenar_recursivo(0, self.__nd, comparador)
+        return self
+
+            
 
 # Metodos extra
 
@@ -282,3 +285,80 @@ class Secuencia(Ic.Conjuntable):
             return a
         else:
             raise StopIteration
+
+    def particion(self, inicio, fin, comparador):
+        """
+        Esta funcion organiza los elementos del directorio de manera que todos los elementos
+        menores o iguales al pivote estan a la izquierda y todos los elementos mayores estan
+        a la derecha. El pivote se coloca en su posicion correcta.
+        :param inicio: La posici�n inicial
+        :param fin: La posicion final
+        :param comparador: El comparador con el que se desea hacer el ordenamiento
+        :return: La posicion correcta del pivote
+        :rtype: int
+        """
+        pivote = self.__datos[inicio]
+        left = inicio + 1
+        right = fin
+        while True:
+            while left <= right and comparador(self.__datos[left], pivote) <= 0:
+                left += 1
+            while comparador(self.__datos[right], pivote) > 0 and right >= left:
+                right -= 1
+            if right < left:
+                break
+            else:  # Intercambiamos los datos que no cumplieron las condiciones
+                self.__datos[left], self.__datos[right] = self.__datos[right], self.__datos[left]
+                # Movemos el pivote a la posici�n correcta
+        self.__datos[inicio], self.__datos[right] = self.__datos[right], self.__datos[inicio]
+        return right  # Devolvemos la posici�n correcta del pivote
+    
+    def ordenar_recursivo(self, inicio, fin, comparador):
+        """
+        Esta funcion aplica recursivamente el algoritmo Quick Sort a los subarreglos definidos por el pivote.
+        :param inicio: La posicion inicial
+        :param fin: La posicion final
+        :param comparador: El comparador con el que se desea hacer el ordenamiento
+        :return: Arreglo de contactos ordenado
+        """
+        if inicio < fin:
+            posicion_part = self.__particion(inicio, fin, comparador)
+            self.ordenar_recursivo(inicio, posicion_part - 1, comparador)
+            self.ordenar_recursivo(posicion_part + 1, fin, comparador)
+        return self.__datos
+
+
+# Metodos comparadores entre empleados.
+def apellido_nombre(a: Em.Empleado, b: Em.Empleado):
+    nombre_1 = Em.a.apellidos()+ ' ' + Em.a.nombre()
+    nombre_2 = b.apellidos()+ ' ' + b.nombre()
+    if nombre_1 < nombre_2:
+        return -1
+    elif nombre_1 > nombre_2:
+        return 1
+    else:
+        return 0
+    
+def edad(a: Em.Empleado, b: Em.Empleado):
+    if a > b:
+        return -1
+    else:
+        return 1
+
+
+def salario_nombre_edad(a: Em.Empleado, b: Em.Empleado) -> bool:
+        dif_salario = a.salario() - b.salario()
+        #Si son iguales bajo el parametro salario
+        if dif_salario == 0:
+            nombre_1 = a.apellidos() + ' ' + a.nombres()
+            nombre_2 = b.apellidos() + ' ' + b.nombres()
+            dif_nombre = apellido_nombre(nombre_1, nombre_2)
+            if dif_nombre == 0:
+                dif_edad = edad(a, b)
+                return dif_edad
+            return dif_nombre
+        return dif_salario
+            
+def numero_empleado(a: Em.Empleado, b: Em.Empleado):
+    return a.num_emp() - b.num_emp()          
+        
